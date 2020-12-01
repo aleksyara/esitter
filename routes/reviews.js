674a0e672067daac15841b64bb7f62d51449e594
Aleksei Yarantsev <aleksyara@Alekseis-MacBook-Pro.local>
2020-12-01 11:06:17 -0800

    add links

--- routes/reviews.js
+++ routes/reviews.js
@@ -5,6 +5,6 @@ const reviewsCtrl = require('../controllers/reviews');
 // /asfdas/asdfasdf/asfdasfd/ 
 
 router.post('/:id', reviewsCtrl.create);
-
+router.delete('/:id', reviewsCtrl.delete);
 
 module.exports = router;
